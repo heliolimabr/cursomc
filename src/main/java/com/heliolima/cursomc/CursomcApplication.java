20522b3e6f0abe59c9357c3e1897085ca4a9daa9
Helio Lima <helio.augustolima@gmail.com>
2018-06-03 17:24:09 -0300

    Creating Cliente, TipoCliente, Telefones and Endereco

--- src/main/java/com/heliolima/cursomc/CursomcApplication.java
+++ src/main/java/com/heliolima/cursomc/CursomcApplication.java
@@ -2,10 +2,15 @@ package com.heliolima.cursomc;
 
 import com.heliolima.cursomc.domain.Categoria;
 import com.heliolima.cursomc.domain.Cidade;
+import com.heliolima.cursomc.domain.Cliente;
+import com.heliolima.cursomc.domain.Endereco;
 import com.heliolima.cursomc.domain.Estado;
 import com.heliolima.cursomc.domain.Produto;
+import com.heliolima.cursomc.domain.enums.TipoCliente;
 import com.heliolima.cursomc.repositories.CategoriaRepository;
 import com.heliolima.cursomc.repositories.CidadeRepository;
+import com.heliolima.cursomc.repositories.ClienteRepository;
+import com.heliolima.cursomc.repositories.EnderecoRepository;
 import com.heliolima.cursomc.repositories.EstadoRepository;
 import com.heliolima.cursomc.repositories.ProdutoRepository;
 import java.util.Arrays;
@@ -28,6 +33,12 @@ public class CursomcApplication implements CommandLineRunner {
     
     @Autowired
     private EstadoRepository estadoRepository;
+    
+    @Autowired
+    private ClienteRepository clienteRepository;
+    
+    @Autowired
+    private EnderecoRepository enderecoRepository;
     
     public static void main(String[] args) {
             SpringApplication.run(CursomcApplication.class, args);
@@ -56,14 +67,26 @@ public class CursomcApplication implements CommandLineRunner {
         Estado est1 = new Estado(null, "Minas Gerais");
         Estado est2 = new Estado(null, "São Paulo");
         
-        Cidade cid1 = new Cidade(null, "Umberlândia", est1);
-        Cidade cid2 = new Cidade(null, "São Paulo", est2);
-        Cidade cid3 = new Cidade(null, "Campinas", est2);
+        Cidade c1 = new Cidade(null, "Umberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
         
-        est1.getCidades().addAll(Arrays.asList(cid1));
-        est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
         
         estadoRepository.saveAll(Arrays.asList(est1, est2));
-        cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+        
+        Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+        
+        cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+        
+        Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+        Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+        
+        cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+        
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2));
     }
 }
---
+++ src/main/java/com/heliolima/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,15 @@
+
+package com.heliolima.cursomc.repositories;
+
+import com.heliolima.cursomc.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Helio Lima
+ */
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    
+}
