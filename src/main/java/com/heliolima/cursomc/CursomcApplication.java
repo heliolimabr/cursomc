4ec0d868b1f62a91ed87a9fee2396672cec016f3
Helio Lima <helio.augustolima@gmail.com>
2018-06-03 15:01:21 -0300

    Initializing data

--- src/main/java/com/heliolima/cursomc/CursomcApplication.java
+++ src/main/java/com/heliolima/cursomc/CursomcApplication.java
@@ -1,12 +1,29 @@
 package com.heliolima.cursomc;
 
+import com.heliolima.cursomc.domain.Categoria;
+import com.heliolima.cursomc.repositories.CategoriaRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    
+    public static void main(String[] args) {
+            SpringApplication.run(CursomcApplication.class, args);
+    }
+
+    //Gerando dados    
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
