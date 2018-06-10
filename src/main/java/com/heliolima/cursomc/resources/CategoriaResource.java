d9f24e3413439a98a0a1f8b6bb373c850a12afaf
Helio Lima <helio.augustolima@gmail.com>
2018-06-10 20:43:08 -0300

    Pagination with optional parameters

--- src/main/java/com/heliolima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/heliolima/cursomc/resources/CategoriaResource.java
@@ -17,9 +17,11 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 /**
  *
@@ -69,6 +71,20 @@ public class CategoriaResource {
         List<Categoria> list = service.findAll();
         //convertendo uma lista em outra
         List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        
+        return ResponseEntity.ok(listDTO);
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/page")
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page, 
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction)
+    {
+        Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+        //convertendo uma lista em outra
+        Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
         
         return ResponseEntity.ok(listDTO);
     }
