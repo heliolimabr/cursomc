0ac489832f4f9123e49a202318784705411abb5e
Helio Lima <helio.augustolima@gmail.com>
2018-06-09 23:18:20 -0300

    Removing unused imports from CategoriaResource

--- src/main/java/com/heliolima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/heliolima/cursomc/resources/CategoriaResource.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.heliolima.cursomc.domain.Categoria;
 import com.heliolima.cursomc.services.CategoriaService;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
