d46fc9e250b813a75a08f9a8ac29ba8b90e9cf43
Helio Lima <helio.augustolima@gmail.com>
2018-06-10 14:07:40 -0300

    Adding a new Category with Post

--- src/main/java/com/heliolima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/heliolima/cursomc/resources/CategoriaResource.java
@@ -12,9 +12,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.heliolima.cursomc.domain.Categoria;
 import com.heliolima.cursomc.services.CategoriaService;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 /**
  *
  * @author Helio Lima
@@ -32,4 +35,13 @@ public class CategoriaResource {
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok(obj);
     }    
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj)
+    {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 }
