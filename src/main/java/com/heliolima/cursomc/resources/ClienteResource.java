1d942ea183887c31a40b3ccaab9ee22220133417
Helio Lima <helio.augustolima@gmail.com>
2018-06-13 21:59:33 -0300

    PUT, DELETE and GET for Cliente

--- src/main/java/com/heliolima/cursomc/resources/ClienteResource.java
+++ src/main/java/com/heliolima/cursomc/resources/ClienteResource.java
@@ -2,12 +2,19 @@
 package com.heliolima.cursomc.resources;
 
 import com.heliolima.cursomc.domain.Cliente;
+import com.heliolima.cursomc.dto.ClienteDTO;
 import com.heliolima.cursomc.services.ClienteService;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,4 +34,43 @@ public class ClienteResource {
         return ResponseEntity.ok(obj);
     }
 
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id)
+    {
+        Cliente obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id)
+    {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    } 
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ClienteDTO>> findAll()
+    {
+        List<Cliente> list = service.findAll();
+        //convertendo uma lista em outra
+        List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+        
+        return ResponseEntity.ok(listDTO);
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/page")
+    public ResponseEntity<Page<ClienteDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page, 
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction)
+    {
+        Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+        //convertendo uma lista em outra
+        Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+        
+        return ResponseEntity.ok(listDTO);
+    }
 }
--- src/main/java/com/heliolima/cursomc/services/ClienteService.java
+++ src/main/java/com/heliolima/cursomc/services/ClienteService.java
@@ -2,10 +2,18 @@
 package com.heliolima.cursomc.services;
 
 import com.heliolima.cursomc.domain.Cliente;
+import com.heliolima.cursomc.domain.Cliente;
+import com.heliolima.cursomc.dto.ClienteDTO;
 import com.heliolima.cursomc.repositories.ClienteRepository;
+import com.heliolima.cursomc.services.exceptions.DataIntegrityException;
 import com.heliolima.cursomc.services.exceptions.ObjectNotFoundException;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 /**
@@ -27,4 +35,42 @@ public class ClienteService {
         );
     }
 
+    public Cliente update(Cliente obj)
+    {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+    
+    public void delete(Integer id)
+    {
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir um cliente que possui pedido.");
+        }
+        
+    }
+    
+    public List<Cliente> findAll(){
+        return repo.findAll();
+    }
+    
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction)
+    {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        
+        return repo.findAll(pageRequest);
+    }
+    
+    public Cliente fromDTO(ClienteDTO objDto){
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+        //throw new UnsupportedOperationException();
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
 }
