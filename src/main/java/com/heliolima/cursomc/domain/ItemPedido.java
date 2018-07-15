45830628ec1dda7e5e2637e524ac1d301ff7c370
Helio Lima <helio.augustolima@gmail.com>
2018-07-15 20:29:36 -0300

    Implementing Pedidos's toString()

--- src/main/java/com/heliolima/cursomc/domain/ItemPedido.java
+++ src/main/java/com/heliolima/cursomc/domain/ItemPedido.java
@@ -3,6 +3,8 @@ package com.heliolima.cursomc.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Objects;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -109,6 +111,22 @@ public class ItemPedido implements Serializable{
             return false;
         }
         return true;
+    }
+
+    @Override
+    public String toString() {
+        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        StringBuilder builder = new StringBuilder();
+        builder.append(getProduto().getNome());
+        builder.append(", Qtde: ");
+        builder.append(getQuantidade());
+        builder.append(", Preço Unitário: ");
+        builder.append(nf.format(getPreco()));
+        builder.append(", Subtotal: ");
+        builder.append(nf.format(getSubTotal()));
+        builder.append("\n");
+        
+        return builder.toString();
     }
     
     
