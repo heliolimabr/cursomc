fc16566766f356f29496c5a63c26fafaf6788f60
Helio Lima <helio.augustolima@gmail.com>
2018-07-16 13:55:13 -0300

    Email HTML

--- src/main/java/com/heliolima/cursomc/services/EmailService.java
+++ src/main/java/com/heliolima/cursomc/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.heliolima.cursomc.services;
 
 import com.heliolima.cursomc.domain.Pedido;
+import javax.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 
 /**
@@ -12,4 +13,8 @@ public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
     
     void sendEmail(SimpleMailMessage msg);
+    
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    
+    void sendHtmlEmail(MimeMessage msg);
 }
